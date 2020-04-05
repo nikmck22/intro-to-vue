2cadd38bf0e5f12f4d0f74dbf7f67bf9b0c886a1
Nikki <nikmck22@gmail.com>
2020-04-05 12:07:33 -0400

    initial commit

---
+++ first.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hello!",
+    today: "Today is Sunday",
+  	questionOne: "What is your mother's maiden name?",
+  	questionTwo: "What is the name of your first pet?"
+},
+  methods: {
+  
+  }
+});
